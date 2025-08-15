15b9739604fea0a10dfb81c9423a777d1f598eb7
CarlosPerez <carlosmperez2901@gmail.com>
2025-08-15 14:31:41 -0700

    Move the fields data to a separated file

---
+++ src/data.js
@@ -0,0 +1,18 @@
+
+export const generalInfoFields = [
+  { name: 'Name', type: 'text', id: crypto.randomUUID() },
+  { name: 'Email', type: 'email', id: crypto.randomUUID() },
+  { name: 'Phone number', type: 'tel', id: crypto.randomUUID() },
+];
+export const educationalExpFields = [
+  { name: 'School name', type: 'text', id: crypto.randomUUID() },
+  { name: 'Title of study', type: 'text', id: crypto.randomUUID() },
+  { name: 'Date of study', type: 'date', id: crypto.randomUUID() },
+];
+export const practicalExpFields = [
+  { name: 'Company name', type: 'text', id: crypto.randomUUID() },
+  { name: 'Position title', type: 'text', id: crypto.randomUUID() },
+  { name: 'Main responsabilities', type: 'text', id: crypto.randomUUID() },
+  { name: 'Start date', type: 'date', id: crypto.randomUUID() },
+  { name: 'End date', type: 'date', id: crypto.randomUUID() }
+];
