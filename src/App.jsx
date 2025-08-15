3d2ba53a844c65a2d1e8e569a786dcd78963b2c6
CarlosPerez <carlosmperez2901@gmail.com>
2025-08-15 13:57:32 -0700

    Add react keys to mapped items and use fieldset to display the section title

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,34 @@
 import './App.css'
 
 import Section from './components/Section.jsx';
+import Button from './components/Button.jsx';
 
 function App() {
 
   const generalInfoFields = [
-    { name: 'Name', type: 'text' },
-    { name: 'Email', type: 'email' },
-    { name: 'Phone number', type: 'tel' },
+    { name: 'Name', type: 'text', id: crypto.randomUUID() },
+    { name: 'Email', type: 'email', id: crypto.randomUUID() },
+    { name: 'Phone number', type: 'tel', id: crypto.randomUUID() },
   ];
   const educationalExpFields = [
-    { name: 'School name', type: 'text' },
-    { name: 'Title of study', type: 'text' },
-    { name: 'Date of study', type: 'date' },
+    { name: 'School name', type: 'text', id: crypto.randomUUID() },
+    { name: 'Title of study', type: 'text', id: crypto.randomUUID() },
+    { name: 'Date of study', type: 'date', id: crypto.randomUUID() },
   ];
   const practicalExpFields = [
-    { name: 'Company name', type: 'text' },
-    { name: 'Position title', type: 'text' },
-    { name: 'Main responsabilities', type: 'text' },
-    { name: 'Start date', type: 'date' },
-    { name: 'End date', type: 'date' }
+    { name: 'Company name', type: 'text', id: crypto.randomUUID() },
+    { name: 'Position title', type: 'text', id: crypto.randomUUID() },
+    { name: 'Main responsabilities', type: 'text', id: crypto.randomUUID() },
+    { name: 'Start date', type: 'date', id: crypto.randomUUID() },
+    { name: 'End date', type: 'date', id: crypto.randomUUID() }
   ];
+
+  function submitHandle() {
+    console.log('Submitted');
+    return (
+      <h1>Submitted</h1>
+    )
+  }
 
 
   return (
@@ -28,6 +36,7 @@ function App() {
       <Section title='General Info' fields={generalInfoFields}></Section>
       <Section title='Educational Exp' fields={educationalExpFields}></Section>
       <Section title='Practical Exp' fields={practicalExpFields}></Section>
+      <Button text='Submit CV' onclickHandle={submitHandle}></Button>
     </>
   )
 }
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -4,13 +4,13 @@ import Field from './Field.jsx';
 export default function Section({ title, fields }) {
   return (
     <section>
-      <h2>{title}</h2>
-      <div class='fields-container'>
-        {fields.map(field =>
-          <Field title={field.name} type={field.type}></Field>
-        )}
-      </div>
-
+      <fieldset>{title}
+        <div className='fields-container'>
+          {fields.map(field =>
+            <Field key={field.id} title={field.name} type={field.type}></Field>
+          )}
+        </div>
+      </fieldset>
     </section>
   )
 }
