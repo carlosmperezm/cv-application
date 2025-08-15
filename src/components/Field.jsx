21a3f9f3c04df2d91c025513c4c57b0d333479aa
CarlosPerez <carlosmperez2901@gmail.com>
2025-08-15 14:47:33 -0700

    Add state to inputs to keep their state after the form is submitted

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,10 +1,16 @@
-
+import { useState } from "react";
 
 export default function Field({ title, type = 'text' }) {
+  const [value, setValue] = useState('');
+
+  function handleKeyPress(e) {
+    setValue(e.target.value);
+  }
+
   return (
     <label>
       {title}:
-      <input type={type} />
+      <input type={type} value={value} onChange={handleKeyPress} />
     </label>
   )
 }
